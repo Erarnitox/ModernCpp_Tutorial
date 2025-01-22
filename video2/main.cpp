39797414bec455f0679ebc3b6fdc8e55d4ab5145
Erarnitox <david@erarnitox.de>
2025-01-22 21:35:18 +0100

    code for video2

---
+++ video2/main.cpp
@@ -0,0 +1,6 @@
+import my_module;
+
+int main() {
+    print_greeting();
+    return 0;
+}
---
+++ video2/my_module.cpp
@@ -0,0 +1,9 @@
+module;
+
+#include <print>
+
+export module my_module;
+
+export void print_greeting() {
+    std::println("Hello Modules!");
+}
